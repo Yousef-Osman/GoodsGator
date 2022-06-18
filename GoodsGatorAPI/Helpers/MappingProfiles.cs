81ae372efb3687090fc1be9e140826688fb7e575
Yousef Osman <yousef.osman.31@gmail.com>
2022-06-18 22:30:18 +0200

    Add auto mapper

---
+++ GoodsGatorAPI/Helpers/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using GoodsGatorAPI.Models.DbEntities;
+using GoodsGatorAPI.Models.DTOs;
+
+namespace GoodsGatorAPI.Helpers;
+
+public class MappingProfiles: Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Product, ProductDTO>()
+            .ForMember(des => des.Brand, op => op.MapFrom(src => src.Brand.Name))
+            .ForMember(des => des.Category, op => op.MapFrom(src => src.Category.Name))
+            .ForMember(des => des.ImageUrl, op => op.MapFrom<ProductUrlResolver>());
+    }
+}
