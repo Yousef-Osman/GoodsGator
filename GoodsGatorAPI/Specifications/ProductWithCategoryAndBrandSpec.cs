9425529ebe85f1ffe2310b76aa4a0f98a936bbc5
Yousef Osman <yousef.osman.31@gmail.com>
2022-06-18 22:29:00 +0200

    Add specification pattern

---
+++ GoodsGatorAPI/Specifications/ProductWithCategoryAndBrandSpec.cs
@@ -0,0 +1,19 @@
+﻿using GoodsGatorAPI.Models.DbEntities;
+using System.Linq.Expressions;
+
+namespace GoodsGatorAPI.Specifications;
+
+public class ProductWithCategoryAndBrandSpec : BaseSpecification<Product>
+{
+    public ProductWithCategoryAndBrandSpec(): base(x => x.IsDeleted == false)
+    {
+        AddInclude(x => x.Brand);
+        AddInclude(x => x.Category);
+    }
+
+    public ProductWithCategoryAndBrandSpec(string id) : base(x => x.Id == id && x.IsDeleted == false)
+    {
+        AddInclude(x => x.Brand);
+        AddInclude(x => x.Category);
+    }
+}
