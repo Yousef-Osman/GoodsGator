55b8bb09e0373105758fdd4a3dc3eeee12b3cb61
Yousef Osman <yousef.osman.31@gmail.com>
2022-08-28 13:11:08 +0200

    Add not found component

--- GoodsGatorSPA/src/app/shared/shared.module.ts
+++ GoodsGatorSPA/src/app/shared/shared.module.ts
@@ -3,18 +3,22 @@ import { CommonModule } from '@angular/common';
 import { PaginationHeaderComponent } from './pagination-header/pagination-header.component';
 import { PaginationFooterComponent } from './pagination-footer/pagination-footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
 @NgModule({
   declarations: [
     PaginationHeaderComponent,
-    PaginationFooterComponent
+    PaginationFooterComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule,
+    AppRoutingModule,
     NgbModule
   ],
-  exports:[PaginationHeaderComponent, PaginationFooterComponent]
+  exports:[PaginationHeaderComponent, PaginationFooterComponent, NotFoundComponent]
 })
 export class SharedModule { }
