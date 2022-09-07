28859b0345cfdce3cc9094a49a2ebd7f82e902ea
Yousef Osman <yousef.osman.31@gmail.com>
2022-09-07 15:43:23 +0200

    Add Basket models

---
+++ GoodsGatorAPI/Models/RedisEntities/BasketItem.cs
@@ -0,0 +1,12 @@
+﻿namespace GoodsGatorAPI.Models.RedisEntities;
+
+public class BasketItem
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+    public string ImageUrl { get; set; }
+    public string Brand { get; set; }
+    public string Category { get; set; }
+}
