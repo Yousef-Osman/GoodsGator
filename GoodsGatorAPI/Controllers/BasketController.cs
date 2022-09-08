71b68861030041926d93c1abb95e7ef4e8ceeba9
Yousef Osman <yousef.osman.31@gmail.com>
2022-09-08 15:48:12 +0200

    Add Basket controller

---
+++ GoodsGatorAPI/Controllers/BasketController.cs
@@ -0,0 +1,37 @@
+﻿using GoodsGatorAPI.Models.RedisEntities;
+using GoodsGatorAPI.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoodsGatorAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class BasketController : ControllerBase
+{
+    private readonly IBasketRepository _basketRepository;
+
+    public BasketController(IBasketRepository basketRepository)
+    {
+        _basketRepository = basketRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetBasketAsync(string id)
+    {
+        var basket = await _basketRepository.GetAsync(id);
+        return Ok(basket ?? new Basket(id));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddOrUpdateBasketAsync(Basket basket)
+    {
+        var dbBasket = await _basketRepository.AddOrUpdateAsync(basket);
+        return Ok(dbBasket);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteBasketAsync(string id)
+    {
+        return Ok(await _basketRepository.DeleteAsync(id));
+    }
+}
