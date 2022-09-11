3207944ba273fd15204d8a0db82a762c9bfe5eff
Yousef Osman <yousef.osman.31@gmail.com>
2022-09-11 21:34:20 +0200

    Add basket UI

--- GoodsGatorSPA/src/app/_basket/basket.component.ts
+++ GoodsGatorSPA/src/app/_basket/basket.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faTrashCan as fasTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { BasketService } from './basket.service';
 
 @Component({
   selector: 'app-basket',
@@ -7,7 +10,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasketComponent implements OnInit {
 
-  constructor() { }
+  constructor(library: FaIconLibrary, private basketService: BasketService) {
+    library.addIcons(fasTrashCan);
+  }
 
   ngOnInit(): void {
   }
