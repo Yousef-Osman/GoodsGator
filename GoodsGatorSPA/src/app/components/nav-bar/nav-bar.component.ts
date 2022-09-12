2ec06a02c824c000f24d06ca3a9a6aab0aef4e90
Yousef Osman <yousef.osman.31@gmail.com>
2022-09-12 15:49:16 +0200

    Add item count in nav bar

--- GoodsGatorSPA/src/app/components/nav-bar/nav-bar.component.ts
+++ GoodsGatorSPA/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faCartShopping as fasCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { Observable } from 'rxjs';
+import { BasketService } from 'src/app/_basket/basket.service';
+import { IBasket } from 'src/app/_shared/interfaces/basket';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,10 +12,13 @@ import { faCartShopping as fasCartShopping } from '@fortawesome/free-solid-svg-i
 })
 export class NavBarComponent implements OnInit {
   
-  constructor(library: FaIconLibrary) {
+  basket$: Observable<IBasket>;
+
+  constructor(library: FaIconLibrary, private basketService: BasketService) {
     library.addIcons(fasCartShopping);
   }
   ngOnInit(): void {
+    this.basket$ = this.basketService.basket$;
   }
 
 }
