968b448826ac8da7c6d6ce657763c704429e039b
Yousef Osman <yousef.osman.31@gmail.com>
2022-09-22 09:34:20 +0200

    Authentication working

--- GoodsGatorAPI/Program.cs
+++ GoodsGatorAPI/Program.cs
@@ -7,12 +7,9 @@ using GoodsGatorAPI.Repositories;
 using GoodsGatorAPI.Repositories.Interfaces;
 using GoodsGatorAPI.Services;
 using GoodsGatorAPI.Services.interfaces;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using StackExchange.Redis;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,25 +24,19 @@ var defaultConnectionString = config.GetConnectionString("DefaultConnection");
 var IdentityConnectionString = config.GetConnectionString("IdentityConnection");
 var redisConnectionString = config.GetConnectionString("RedisConnection");
 
-
 //add database files 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(defaultConnectionString));
 builder.Services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(IdentityConnectionString));
-
 
 //add identity 
 builder.Services.AddIdentityServices(config);
 
 //to add redis database
-builder.Services.AddIdentity<AppUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddEntityFrameworkStores<AppIdentityDbContext>().AddSignInManager<SignInManager<AppUser>>();
-
-//to add redis
 builder.Services.AddSingleton<IConnectionMultiplexer>(c =>
 {
     var configuration = ConfigurationOptions.Parse(redisConnectionString, true);
     return ConnectionMultiplexer.Connect(configuration);
-}); 
+});
 
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
@@ -55,20 +46,8 @@ builder.Services.AddAutoMapper(typeof(MappingProfiles));
 
 builder.Services.AddCors(options => options.AddPolicy("CorsPolicy", policy => policy
     .WithOrigins(config.GetSection("ClientUrl").Value)
-    .WithOrigins(builder.Configuration.GetSection("ClientUrl").Value)
     .AllowAnyHeader()
     .AllowAnyMethod()));
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Token:Key").Value)),
-        ValidIssuer = builder.Configuration.GetSection("Token:Issuer").Value,
-        ValidateIssuer = true
-    };
-});
 
 //to override the validation behavior of [ApiController] attribute
 builder.Services.AddApplicationServices();
@@ -109,6 +88,7 @@ app.SeedData();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
