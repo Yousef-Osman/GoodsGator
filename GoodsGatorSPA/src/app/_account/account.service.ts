ff5ec409233451ddca4c250d5ca4d2533552daea
Yousef Osman <yousef.osman.31@gmail.com>
2022-10-05 15:37:30 +0200

    Minor UI update in SPA

--- GoodsGatorSPA/src/app/_account/account.service.ts
+++ GoodsGatorSPA/src/app/_account/account.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Route } from '@angular/router';
+import { BehaviorSubject, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IUser } from '../_shared/interfaces/iUser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  baseUrl = environment.apiUrl + 'account/';
+  currentUserSource = new BehaviorSubject<IUser>(null);
+  currentUser$ = this.currentUserSource.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Route) { }
+
+  login(creds: any) {
+    return this.createRequest(creds, 'login');
+  }
+
+  register(creds: any) {
+    return this.createRequest(creds, 'register');
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.currentUserSource.next(null);
+  }
+
+  checkEmail(email:string) {
+    return this.http.get(this.baseUrl + 'checkemail?email=' + email);
+  }
+
+  createRequest(creds: any, action: string) {
+    return this.http.post(this.baseUrl + action, creds).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
 }
